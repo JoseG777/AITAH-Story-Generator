bc46fc4a0d51768dc3f959e000ac27f5fb952a7a
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-07-16 01:52:23 -0400

    added imports needed for main.py, initialized tools and models

--- main.py
+++ main.py
@@ -1,35 +1,19 @@
-import os
-from datetime import date
+# from datetime import date
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-import openai
-from tavily import TavilyClient
+from langchain_community.tools.tavily_search import TavilySearchResults
 
+# from langchain_core.messages import HumanMessage
+# from langgraph.graph import StateGraph, END
 # from agents.summary_agent import SummarizationAgent
 # from agents.validation_agent import CoherenceAgent
-# from langchain_core.messages import HumanMessage
-# from langgraph.graph import END, StateGraph, MessagesState
-# from langgraph.checkpoint import MemorySaver
+# from agents.base_agent import AgentState
 
 
 load_dotenv()
 
-# OpenAI set up
-openai_api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = openai_api_key
+# Tavily tool set up
+tavily_tool = TavilySearchResults(max_results=5, search_depth="advanced")
 
+# Initialize the OpenAI model
 model = ChatOpenAI(model="gpt-4o")
-
-# Tavily set up
-current_date = date.today()
-tavily_api_key = os.getenv("TAVILY_API_KEY")
-tavily = TavilyClient(tavily_api_key)
-response = tavily.search(
-    f"What are the most important pieces of tech news as of {current_date}",
-    max_results=5,
-    search_depth="advanced",
-)
-
-responses = [response["content"] for response in response["results"]]
-
-print(responses)
