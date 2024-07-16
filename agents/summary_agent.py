f460eec8992d7f0072512d7fd5d5379189b30532
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-07-16 01:39:17 -0400

    updated summary agent to include tavily search

--- agents/summary_agent.py
+++ agents/summary_agent.py
@@ -1,17 +1,29 @@
 from langgraph.graph import StateGraph, END
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage, SystemMessage, ToolMessage
 from agents.base_agent import AgentState
 
 
 class SummarizationAgent:
-    def __init__(self, model, system=""):
+    def __init__(self, model, tools, system=""):
         self.system = system
         graph = StateGraph(AgentState)
+        graph.add_node("search", self.search_with_tavily)
         graph.add_node("summarize", self.summarize_tool)
+        graph.add_edge("summarize", "search")
         graph.add_edge("summarize", END)
-        graph.set_entry_point("summarize")
+        graph.set_entry_point("search")
         self.graph = graph.compile()
         self.model = model
+        self.tools = tools
+
+    def search_with_tavily(self, state: AgentState) -> dict:
+        """Use Tavily to search for tech news."""
+        messages = state["messages"]
+        query = messages[-1].content
+        tool = self.tools["tavily_search_results"]
+        results = tool.invoke({"query": query})
+        combined_responses = " ".join(result["content"] for result in results)
+        return {"messages": [ToolMessage(content=combined_responses)]}
 
     def summarize_tool(self, state: AgentState) -> dict:
         """Summarize the provided text using OpenAI."""
